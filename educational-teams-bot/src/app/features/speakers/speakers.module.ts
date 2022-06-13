d786511589c24898a33b3f59aa1727b7d3cb2066
Geoffrey Lioté <geoffrey.liote@diiage.org>
2022-06-13 14:16:11 +0200

    merge question to speakers

--- educational-teams-bot/src/app/features/speakers/speakers.module.ts
+++ educational-teams-bot/src/app/features/speakers/speakers.module.ts
@@ -2,9 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SpeakersComponent } from './components/speakers/speakers.component';
 import { SpeakerItemComponent } from './components/speaker-item/speaker-item.component';
-import { AutoTableComponent } from './components/auto-table/auto-table.component';
-import { AutoListComponent } from './components/auto-list/auto-list.component';
-import { AutoUpsertComponent } from './components/auto-upsert/auto-upsert.component';
+import { AutoUpsertComponent } from '../../shared/components/auto-upsert/auto-upsert.component';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
@@ -12,7 +10,9 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatDialogModule } from '@angular/material/dialog';
-import { AutoDeleteComponent } from './components/auto-delete/auto-delete.component';
+import { AutoDeleteComponent } from 'src/app/shared/components/auto-delete/auto-delete.component';
+import { AutoListComponent } from 'src/app/shared/components/auto-list/auto-list.component';
+import { AutoTableComponent } from 'src/app/shared/components/auto-table/auto-table.component';
 
 
 
